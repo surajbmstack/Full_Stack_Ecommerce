0320bca593eaa3192d30b17e1db24e43510e81a4
surajbmstack <surajbmsteam@gmail.com>
2024-07-28 18:29:09 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import men_banner from "./Components/Assets/banner_mens.png";
 import kid_banner from "./Components/Assets/banner_kids.png";
 import LoginSignup from "./Pages/LoginSignup";
 
-export const backend_url = 'http://localhost:4000';
+export const backend_url = 'https://full-stack-ecommerce-pd07.onrender.com';
 export const currency = '₹';
 
 function App() {
